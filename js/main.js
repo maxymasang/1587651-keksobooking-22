7b184737b0fcc496a7c4b326cc79c878a0122151
Maxym <maxymasan.html@gmail.com>
2021-02-01 16:52:56 +0200

    Работа с функциями

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
+// Временная функция
+const getRandomIntNumber = function (min, max) {
+  if (min >= max) {
+    alert('Некорректно введены данные, значение (min) должно быть меньше значения (max)!');
+  }
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Функция для проверки максимальной длины строки
+// Функция из другого домашнего задания
+const checkMaxStringLenght = function (checkedString, maxLength) {
+  if (checkedString.length > maxLength) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+checkMaxStringLenght('Test text', 3);
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+// Генерации временных географических координат в следующем задании
+const getRandomFloatNumber = function (min, max, signsNumber) {
+  if (min >= max) {
+    alert('Некорректно введены данные, значение (min) должно быть меньше значения (max)!');
+  }
+  return (Math.random() * (max - min) + min).toFixed(signsNumber);
+}
+
+
