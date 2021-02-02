cd31b76427c4611f95f4071f552c8c92f5e4f037
Maxym <maxymasan.html@gmail.com>
2021-02-02 11:40:29 +0200

    Работа с функциями, правки

--- js/main.js
+++ js/main.js
@@ -1,35 +1,41 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно
-// Временная функция
+/**
+ * Функция, возвращающая случайное целое число из переданного диапазона включительно
+ * @param {number} min - минимальное число
+ * @param {number} max - максимальное число
+ * @returns {number}
+ */
 const getRandomIntNumber = function (min, max) {
   if (min >= max) {
-    alert('Некорректно введены данные, значение (min) должно быть меньше значения (max)!');
+    throw new Error('Значение (min) должно быть меньше значения (max)');
   }
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
 getRandomIntNumber(4, 10);
 
-// Функция для проверки максимальной длины строки
-// Функция из другого домашнего задания
+/**
+ * Функция для проверки максимальной длины строки
+ * @param {string} checkedString - строка
+ * @param {number} maxLength - максимальная длина строки
+ * @returns {boolean}
+ */
 const checkMaxStringLenght = function (checkedString, maxLength) {
-  if (checkedString.length > maxLength) {
-    return false;
-  }
-  else {
-    return true;
-  }
+  return checkedString.length < maxLength;
 }
 
 checkMaxStringLenght('Test text', 3);
 
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-// Генерации временных географических координат в следующем задании
+/**
+ * Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+ * @param {*} min - минимальное число
+ * @param {*} max - максимальное число
+ * @param {*} signsNumber - количество символов после запятых
+ */
 const getRandomFloatNumber = function (min, max, signsNumber) {
   if (min >= max) {
-    alert('Некорректно введены данные, значение (min) должно быть меньше значения (max)!');
+    throw new Error('Значение (min) должно быть меньше значения (max)');
   }
-  return (Math.random() * (max - min) + min).toFixed(signsNumber);
+  return Number((Math.random() * (max - min) + min).toFixed(signsNumber));
 }
 
-getRandomFloatNumber(1.1, 2.3);
-
+getRandomFloatNumber(1.1, 2.3, 5);
